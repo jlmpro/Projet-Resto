d0b4c1acfa7234cc4921ef518a1da7731396b669
Jean Moris <anatojeanmoris@gmail.com>
2025-05-09 14:58:00 +0000

    Démo controller

--- Backend/resto/src/main/java/com/moris/resto/demo/DemoController.java
+++ Backend/resto/src/main/java/com/moris/resto/demo/DemoController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/demo-controller")
+@RequestMapping
 public class DemoController {
 
-  @GetMapping
+  @GetMapping("/demo-controller")
   public ResponseEntity<String> sayHello() {
     return ResponseEntity.ok("Hello from secured endpoint");
   }
